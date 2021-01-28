916078356f5941feed2dbf332b5b44f5fc71b5ab
Jon <jongrabbe@outlook.com>
2021-01-27 22:22:09 -0800

    first commit

---
+++ src/start/start-header.js
@@ -0,0 +1,7 @@
+export default function StartHeader() {
+    return(
+        <header>
+            <h1>Trivia Quiz</h1>
+        </header>
+    )
+}
---
+++ src/start/start.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './start-header';
+
+export default class Start extends React.Component {
+    render() {
+        return(
+            <div className="start">
+                <Header />
+            </div>        
+        )
+        
+    }
+}
